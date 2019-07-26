aa5b78a9d6174da2514acf4e153fe1f3d749f269
Breno Calazans <brenocalazans@gmail.com>
2019-07-26 16:04:02 -0300

    Send addToCart and removeFromCart events

--- react/typings/global.d.ts
+++ react/typings/global.d.ts
@@ -1,3 +1,13 @@
 interface Window extends Window {
-  __bluecore_site_id: string
+  __bluecore_site_id: string;
+  triggermail: Bluecore;
 }
+
+interface Bluecore {
+  Partner: {
+    dataReady(params: AddToCart | RemoveFromCart): void;
+  };
+}
+
+type AddToCart = { event_type: "add_to_cart"; id: string };
+type RemoveFromCart = { event_type: "remove_from_cart"; id: string };
